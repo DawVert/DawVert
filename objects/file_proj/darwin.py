0bee0b5e2d1b3c62576fd41140488531aebb42f0
SatyrDiamond <satyrdiamond@satyrdiamond.org>
2026-03-08 12:11:50 -0400

    darwin

---
+++ objects/file_proj/darwin.py
@@ -0,0 +1,143 @@
+# SPDX-FileCopyrightText: 2026 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import json
+
+class darwin_note:
+	def __init__(self):
+		self.durationTicks = 0
+		self.pitch = 60
+		self.startTick = 0
+		self.velocity = 100
+
+	def read(self, trackdata):
+		if 'durationTicks' in trackdata: self.durationTicks = trackdata['durationTicks']
+		if 'pitch' in trackdata: self.pitch = trackdata['pitch']
+		if 'startTick' in trackdata: self.startTick = trackdata['startTick']
+		if 'velocity' in trackdata: self.velocity = trackdata['velocity']
+
+	def write(self):
+		out = {}
+		out['durationTicks'] = self.durationTicks
+		out['pitch'] = self.pitch
+		out['startTick'] = self.startTick
+		out['velocity'] = self.velocity
+		return out
+
+class darwin_clip:
+	def __init__(self):
+		self.clipType = "midi"
+		self.durationTicks = 128
+		self.notes = []
+		self.startTick = 0
+
+	def read(self, trackdata):
+		if 'clipType' in trackdata: self.clipType = trackdata['clipType']
+		if 'durationTicks' in trackdata: self.durationTicks = trackdata['durationTicks']
+		if 'startTick' in trackdata: self.startTick = trackdata['startTick']
+		if 'notes' in projectdata: 
+			for t in projectdata['notes']:
+				note_obj = darwin_note()
+				note_obj.read(t)
+				self.notes.append(note_obj)
+
+	def write(self):
+		out = {}
+		out['clipType'] = self.clipType
+		out['durationTicks'] = self.durationTicks
+		out['notes'] = [x.write() for x in self.notes]
+		out['startTick'] = self.startTick
+		return out
+
+class darwin_track:
+	def __init__(self):
+		self.clips = []
+		self.color = "#888888"
+		self.folderExpanded = True
+		self.id = 0
+		self.instrumentName = ""
+		self.isFolder = False
+		self.muted = False
+		self.name = "Track"
+		self.pan = 0
+		self.parentFolderId = -1
+		self.solo = False
+		self.timingOffsetMs = 0
+		self.visible = True
+		self.volume = 1
+
+	def read(self, trackdata):
+		if 'clips' in projectdata: 
+			for t in projectdata['clips']:
+				clip_obj = darwin_clip()
+				clip_obj.read(t)
+				self.clips.append(clip_obj)
+		if 'color' in trackdata: self.color = trackdata['color']
+		if 'folderExpanded' in trackdata: self.folderExpanded = trackdata['folderExpanded']
+		if 'id' in trackdata: self.id = trackdata['id']
+		if 'instrumentName' in trackdata: self.instrumentName = trackdata['instrumentName']
+		if 'isFolder' in trackdata: self.isFolder = trackdata['isFolder']
+		if 'muted' in trackdata: self.muted = trackdata['muted']
+		if 'name' in trackdata: self.name = trackdata['name']
+		if 'pan' in trackdata: self.pan = trackdata['pan']
+		if 'parentFolderId' in trackdata: self.parentFolderId = trackdata['parentFolderId']
+		if 'solo' in trackdata: self.solo = trackdata['solo']
+		if 'timingOffsetMs' in trackdata: self.timingOffsetMs = trackdata['timingOffsetMs']
+		if 'visible' in trackdata: self.visible = trackdata['visible']
+		if 'volume' in trackdata: self.volume = trackdata['volume']
+
+	def write(self):
+		out = {}
+		out['clips'] = [x.write() for x in self.clips]
+		out['color'] = self.color
+		out['folderExpanded'] = self.folderExpanded
+		out['id'] = self.id
+		out['instrumentName'] = self.instrumentName
+		out['isFolder'] = self.isFolder
+		out['muted'] = self.muted
+		out['name'] = self.name
+		out['pan'] = self.pan
+		out['parentFolderId'] = self.parentFolderId
+		out['solo'] = self.solo
+		out['timingOffsetMs'] = self.timingOffsetMs
+		out['visible'] = self.visible
+		out['volume'] = self.volume
+		return out
+
+class darwin_project:
+	def __init__(self):
+		self.bpm = 128
+		self.formatVersion = 1
+		self.masterTrack = darwin_track()
+		self.name = ""
+		self.tracks = []
+
+	def load_from_file(self, input_file):
+		f = open(input_file, 'rb')
+		projectdata = json.load(f)
+		self.read(projectdata)
+		return True
+
+	def read(self, projectdata):
+		if 'bpm' in projectdata: self.bpm = projectdata['bpm']
+		if 'formatVersion' in projectdata: self.formatVersion = projectdata['formatVersion']
+		if 'name' in projectdata: self.masterTrack = projectdata['name']
+		if 'masterTrack' in projectdata: self.masterTrack.read(projectdata['masterTrack'])
+		if 'tracks' in projectdata: 
+			for t in projectdata['tracks']:
+				track_obj = darwin_track()
+				track_obj.read(t)
+				self.tracks.append(track_obj)
+
+	def write(self):
+		out = {}
+		out['bpm'] = self.bpm
+		out['formatVersion'] = self.formatVersion
+		out['name'] = self.name
+		out['masterTrack'] = self.masterTrack.write()
+		out['tracks'] = [x.write() for x in self.tracks]
+		return out
+
+	def save_to_file(self, output_file):
+		f = open(output_file, 'w')
+		f.write(json.dumps(self.write(), indent = 2))
---
+++ plugins/output/darwin.py
@@ -0,0 +1,75 @@
+# SPDX-FileCopyrightText: 2026 SatyrDiamond
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import plugins
+import os
+from objects import globalstore
+from functions import xtramath
+
+def to_color(visual_obj):
+	return '#'+visual_obj.color.get_hex()
+
+class output_darwin(plugins.base):
+	def is_dawvert_plugin(self):
+		return 'output'
+	
+	def get_shortname(self):
+		return 'darwin'
+	
+	def get_name(self):
+		return 'darwin'
+	
+	def gettype(self):
+		return 'r'
+	
+	def get_prop(self, in_dict): 
+		in_dict['projtype'] = 'r'
+		in_dict['fxtype'] = 'none'
+		in_dict['file_ext'] = 'darwin'
+
+	def parse(self, convproj_obj, dawvert_intent):
+		from objects.file_proj import darwin as proj_darwin
+
+		convproj_obj.change_timings(480)
+		
+		project_obj = proj_darwin.darwin_project()
+		project_obj.bpm = int(convproj_obj.params.get('bpm', 120).value)
+		project_obj.masterTrack.volume = convproj_obj.track_master.params.get('vol', 1).value
+
+		tracknum = 0
+		for trackid, track_obj in convproj_obj.track__iter():
+			if track_obj.type in ['instrument']:
+				visual_obj = track_obj.visual
+				visual_inst_obj = track_obj.visual_inst
+
+				darwin_track = proj_darwin.darwin_track() 
+				darwin_track.id = tracknum
+				if visual_obj.color: darwin_track.color = to_color(outcolor)
+				elif visual_inst_obj.color: darwin_track.color = to_color(visual_inst_obj)
+				if visual_obj.name: darwin_track.name = visual_obj.name
+				darwin_track.muted = not bool(track_obj.params.get('on', 1).value)
+				darwin_track.pan = track_obj.params.get('pan', 1).value
+				darwin_track.solo = bool(track_obj.params.get('solo', 1).value)
+				darwin_track.volume = track_obj.params.get('vol', 1).value
+
+				for notespl_obj in track_obj.placements.pl_notes:
+					position, duration = notespl_obj.time.get_posdur()
+					clip_obj = proj_darwin.darwin_clip()
+					clip_obj.startTick = int(position)
+					clip_obj.durationTicks = int(duration)
+
+					for cnote in notespl_obj.notelist.iter_notes():
+						note_obj = proj_darwin.darwin_note()
+						note_obj.startTick = int(cnote.pos)
+						note_obj.durationTicks = int(cnote.dur)
+						note_obj.pitch = int(cnote.key)+60
+						note_obj.velocity = int(float(cnote.vol)*100)
+						clip_obj.notes.append(note_obj)
+
+					darwin_track.clips.append(clip_obj)
+
+				project_obj.tracks.append(darwin_track)
+				tracknum += 1
+
+		if dawvert_intent.output_mode == 'file':
+			project_obj.save_to_file(dawvert_intent.output_file)
