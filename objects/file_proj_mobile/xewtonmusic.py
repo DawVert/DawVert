b556daa76493f91f87f93e87afbe693c9d48c6f0
SatyrDiamond <satyrdiamond@satyrdiamond.org>
2026-04-25 15:07:07 -0400

    xewton

--- objects/file_proj_mobile/xewtonmusic.py
+++ objects/file_proj_mobile/xewtonmusic.py
@@ -4,7 +4,9 @@
 from external.easybinrw import easybinrw
 import numpy as np
 
-class xewtonmusic_track:
+# =========================================== SONG ===========================================
+
+class xewtonmusic_song_track:
 	def __init__(self):
 		self.unk = []
 		self.tracknum = 0
@@ -28,7 +30,7 @@ class xewtonmusic_track:
 		self.volume = ebrw_readstr.float()
 		self.pan = ebrw_readstr.float()
 
-class xewtonmusic_header:
+class xewtonmusic_song_header:
 	def __init__(self):
 		self.unk = []
 		self.seconds = 0
@@ -51,7 +53,7 @@ class xewtonmusic_header:
 		#self.unk.append(  ebrw_readstr.int_u16()  )
 		#self.unk.append(  ebrw_readstr.int_u16()  )
 
-#class xewtonmusic_effx:
+#class xewtonmusic_song_effx:
 #	def __init__(self):
 #		self.unk = []
 
@@ -66,7 +68,7 @@ notes_dtype = np.dtype([
 	('unk', np.uint16),
 	])
 
-class xewtonmusic_note:
+class xewtonmusic_song_note:
 	def __init__(self):
 		self.pos = 0
 		self.dur = 0
@@ -84,7 +86,7 @@ class xewtonmusic_note:
 		self.unk2 = ebrw_readstr.int_u8()
 		if self.unk1: self.unk1_data = ebrw_readstr.raw(self.unk1*4)
 
-class xewtonmusic_notes:
+class xewtonmusic_song_notes:
 	def __init__(self):
 		self.tracknum = 0
 		self.notes = []
@@ -94,14 +96,14 @@ class xewtonmusic_notes:
 		self.unk = ebrw_readstr.int_u16()
 		num_notes = ebrw_readstr.int_u32()
 		for n in range(num_notes):
-			note = xewtonmusic_note()
+			note = xewtonmusic_song_note()
 			note.read(ebrw_readstr)
 			self.notes.append(note)
 
-class xewtonmusic_file:
+class xewtonmusic_song_file:
 	def __init__(self):
 		self.tracks = {}
-		self.header = xewtonmusic_header()
+		self.header = xewtonmusic_song_header()
 
 	def load_from_file(self, input_file):
 		ebrw_readstr = easybinrw.binread()
@@ -118,18 +120,18 @@ class xewtonmusic_file:
 			ebrw_readstr.isolate_size(chunk_size)
 
 			if chunk_name==b'TINF':
-				tinf_obj = xewtonmusic_track()
+				tinf_obj = xewtonmusic_song_track()
 				tinf_obj.read(ebrw_readstr)
 				self.tracks[tinf_obj.tracknum] = tinf_obj
 			elif chunk_name==b'TNOT':
-				tnot_obj = xewtonmusic_notes()
+				tnot_obj = xewtonmusic_song_notes()
 				tnot_obj.read(ebrw_readstr)
 				if tnot_obj.tracknum in self.tracks:
 					self.tracks[tnot_obj.tracknum].notes = tnot_obj
 			elif chunk_name==b'HEAD':
 				self.header.read(ebrw_readstr)
 			#elif chunk_name==b'EFFX':
-			#	tinf_obj = xewtonmusic_effx()
+			#	tinf_obj = xewtonmusic_song_effx()
 			#	tinf_obj.read(ebrw_readstr)
 			#	#exit()
 			#else:
@@ -137,4 +139,114 @@ class xewtonmusic_file:
 			#	chunk_data = ebrw_readstr.raw(chunk_size)
 
 			ebrw_readstr.isolate_end()
+		return True
+
+# =========================================== INST ===========================================
+
+def read_string(ebrw_readstr):
+	nsize = ebrw_readstr.int_u16()
+	return ebrw_readstr.string16(nsize)
+
+class xewtonmusic_inst_header:
+	def __init__(self):
+		self.unk = []
+		self.unkf = []
+		self.attack = 0
+		self.release = 0
+
+	def read(self, ebrw_readstr):
+		self.unk = []
+		self.unkf = []
+		self.version = ebrw_readstr.int_u16()
+		self.num_regions = ebrw_readstr.int_u16()
+		self.instnum = ebrw_readstr.int_u16()
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u16()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unkf.append(  ebrw_readstr.float()  )
+		self.attack = ebrw_readstr.float()
+		self.release = ebrw_readstr.float()
+		self.unkf.append(  ebrw_readstr.float()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		#print(self.unkf)
+
+class xewtonmusic_inst_sample:
+	def __init__(self):
+		self.unk = []
+		self.unkf = []
+		self.filename = ''
+		self.name = ''
+		self.name2 = ''
+		self.key_root = 0
+		self.key_lo = 0
+		self.key_hi = 0
+		self.loop_on = 0
+		self.loop_1 = 0
+		self.loop_2 = 0
+		self.loop_3 = 0
+		self.data = None
+		self.vol = 1
+
+	def read(self, ebrw_readstr):
+		self.unk = []
+		self.unkf = []
+		self.filename = read_string(ebrw_readstr)
+		self.name = read_string(ebrw_readstr)
+		self.name2 = read_string(ebrw_readstr)
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.unk.append(  ebrw_readstr.int_u8()  )
+		self.key_root = ebrw_readstr.int_u8()
+		self.key_lo = ebrw_readstr.int_u8()
+		self.key_hi = ebrw_readstr.int_u8()
+		self.vol = ebrw_readstr.float()
+		self.unkf.append(  ebrw_readstr.float()  )
+		self.loop_on = ebrw_readstr.int_u8()
+		self.unkf.append(  ebrw_readstr.float()  )
+		self.loop_1 = ebrw_readstr.int_u32()
+		self.loop_2 = ebrw_readstr.int_u32()
+		self.loop_3 = ebrw_readstr.int_u32()
+
+class xewtonmusic_inst_file:
+	def __init__(self):
+		self.name = None
+		self.header = xewtonmusic_inst_header()
+		self.samples = []
+
+	def load_from_file(self, input_file):
+		ebrw_readstr = easybinrw.binread()
+		ebrw_readstr.load_file(input_file)
+
+		ebrw_readstr.magic_check(b'INST')
+		ebrw_readstr.skip(4)
+
+		self.samples = []
+		while ebrw_readstr.remaining():
+			chunk_name = ebrw_readstr.raw(4)
+			chunk_size = ebrw_readstr.int_u32()
+
+			ebrw_readstr.isolate_size(chunk_size)
+
+			if chunk_name==b'HEAD':
+				self.header.read(ebrw_readstr)
+			elif chunk_name==b'NAME':
+				self.name = read_string(ebrw_readstr)
+			elif chunk_name==b'SINF':
+				sample_obj = xewtonmusic_inst_sample()
+				sample_obj.read(ebrw_readstr)
+				self.samples.append(sample_obj)
+			elif chunk_name==b'SDAT':
+				sample_obj.data = ebrw_readstr.rest()
+
+			ebrw_readstr.isolate_end()
+
 		return True
--- plugins/input_mobile/r_xewtonmusic.py
+++ plugins/input_mobile/r_xewtonmusic.py
@@ -4,6 +4,8 @@
 import plugins
 from functions import xtramath
 from objects.convproj import fileref
+from objects import globalstore
+import os
 
 class input_xewton(plugins.base):
 	def is_dawvert_plugin(self):
@@ -24,8 +26,9 @@ class input_xewton(plugins.base):
 
 	def parse(self, convproj_obj, dawvert_intent):
 		from objects.file_proj_mobile import xewtonmusic
+		from objects import audio_data
 
-		project_obj = xewtonmusic.xewtonmusic_file()
+		project_obj = xewtonmusic.xewtonmusic_song_file()
 
 		convproj_obj.type = 'r'
 
@@ -42,6 +45,12 @@ class input_xewton(plugins.base):
 		convproj_obj.params.add('bpm', project_obj.header.tempo, 'float')
 		convproj_obj.timesig = [project_obj.header.timesig1, project_obj.header.timesig2]
 		
+		globalstore.datapack.load('xewton', './data/datapack/app/xewton.xml')
+
+		extpath_path = os.path.join(dawvert_intent.path_external_data, 'xewton')
+		samplefolder = dawvert_intent.path_samples['extracted']
+
+		instplugs = {}
 
 		for tracknim, xe_tr in project_obj.tracks.items():
 			idval = 'track'+str(tracknim)
@@ -51,17 +60,73 @@ class input_xewton(plugins.base):
 			track_obj.params.add('pan', xe_tr.pan, 'float')
 
 			visual_obj = track_obj.visual
+			visual_obj.from_datapack('xewton', 'inst', str(xe_tr.instnum), True)
+
 			if xe_tr.color is not None: 
 				cr = xe_tr.color[2]
 				cg = xe_tr.color[1]
 				cb = xe_tr.color[0]
 				visual_obj.color.set_int([cr, cg, cb])
 
-			cvpj_notelist = track_obj.placements.notelist
+			if xe_tr.instnum not in instplugs:
+				instplug = 'inst'+str(xe_tr.instnum)
+				plugin_obj = convproj_obj.plugin__add(instplug, 'universal', 'sampler', 'multi')
+				instplugs[xe_tr.instnum] = [plugin_obj, [track_obj]]
+			else:
+				instplugs[xe_tr.instnum][1].append(track_obj)
+			track_obj.plugslots.set_synth(instplug)
 
+			cvpj_notelist = track_obj.placements.notelist
 			tracknotes = xe_tr.notes.notes
 			for n in tracknotes:
 				cvpj_notelist.add_r(int(n.pos), int(n.dur), n.key-60, n.vol/127, None)
+
+		for instnum, instdata in instplugs.items():
+			plugin_obj, tracks = instdata
+			dp_obj = globalstore.datapack.get_obj('xewton', 'inst', str(instnum))
+			if dp_obj is not None:
+				dpdata = dp_obj.data
+				filename = dpdata['filename'] if 'filename' in dpdata else None
+				if filename:
+					instfilename = os.path.join(extpath_path, filename)
+					if os.path.exists(instfilename):
+						inst_obj = xewtonmusic.xewtonmusic_inst_file()
+						inst_obj.load_from_file(instfilename)
+
+						plugin_obj.env_asdr_add('vol', 0, inst_obj.header.attack, 0, 0, 1, inst_obj.header.release, 1)
+
+						isdrums = max([s.key_hi-s.key_lo for s in inst_obj.samples])==0
+
+						if isdrums:
+							for t in tracks:
+								t.is_drum = True
+
+						for s in inst_obj.samples:
+							wavfilename = os.path.join(samplefolder, s.filename)
+							org_filename =  s.filename
+
+							sampleref_obj = convproj_obj.sampleref__add(s.filename, wavfilename, None)
+							audio_obj = audio_data.audio_obj()
+							audio_obj.rate = 22050
+							audio_obj.channels = 2
+							audio_obj.set_codec('int16')
+							audio_obj.pcm_from_bytes(s.data)
+							audio_obj.to_file_wav(wavfilename)
+							sampleref_obj.set_fileformat('wav')
+							audio_obj.to_sampleref_obj(sampleref_obj)
+
+							#print(sampleref_obj.get_dur_samples(), s.loop_on, s.loop_1//2, s.loop_2//2, s.loop_3//2, s.filename)
+
+							sp_obj = plugin_obj.sampleregion_add(s.key_lo-60, s.key_hi-60, s.key_root-60, None)
+							sp_obj.point_value_type = "samples"
+							sp_obj.sampleref = s.filename
+							sp_obj.start = 0
+							sp_obj.vol = s.vol
+							sp_obj.end = sampleref_obj.get_dur_samples()
+							#sp_obj.loop_start = s.loop_1
+							sp_obj.loop_end = sp_obj.end
+							sp_obj.loop_active = bool(s.loop_on)
+							sp_obj.trigger = 'oneshot' if isdrums else 'normal'
 
 		convproj_obj.do_actions.append('do_addloop')
 		convproj_obj.do_actions.append('do_singlenotelistcut')
