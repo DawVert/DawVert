61c1bb98f5fdb3fe12ec35bd433c3cc2178ca515
SatyrDiamond <satyrdiamond@satyrdiamond.org>
2026-05-23 19:24:22 -0400

    update

--- plugins/input_2010s/r_sequel3.py
+++ plugins/input_2010s/r_sequel3.py
@@ -196,8 +196,6 @@ class input_sequel3(plugins.base):
 
 		samplefolder = dawvert_intent.path_samples['extracted']
 
-		globalstore.datapack.load('z_maestro', './data/datapack/app/z_maestro.xml')
-
 		convproj_obj.type = 'r'
 		convproj_obj.fxtype = 'groupreturn'
 
